72a19d2fefd57f5b7b028dbf617d47b244386fda
Charles Humphries <charles.humphries.jr@gmail.com>
2015-08-31 19:48:29 -0400

    Waves and Tiles Changes
    Added enemy waves and ability to change tiles by mousing over

--- GameProject/src/data/Boot.java
+++ GameProject/src/data/Boot.java
@@ -2,10 +2,7 @@ package data;
 
 import static helpers.Artist.*;
 import helpers.Clock;
-
-
 import org.lwjgl.opengl.Display;
-import org.newdawn.slick.opengl.Texture;
 
 public class Boot 
 {
@@ -35,14 +32,16 @@ public class Boot
 		
 		TileGrid grid = new TileGrid(map);
 		grid.setTile(3, 4, grid.getTile(5, 7).getType());
-		Enemy e = new Enemy(quickLoad("enemy"), grid.getTile(10, 10), 64, 64, 1);
+		Enemy e = new Enemy(quickLoad("enemy"), grid.getTile(10, 10), 64, 64, 6);
+		Wave wave = new Wave(20, e);
+		Player player = new Player(grid);
 		while(!Display.isCloseRequested())
 		{
 			Clock.update();
-			e.Update();
 			
 			grid.draw();
-			e.Draw();
+			wave.Update();
+			player.Update();
 			
 			Display.update();
 			Display.sync(60);
